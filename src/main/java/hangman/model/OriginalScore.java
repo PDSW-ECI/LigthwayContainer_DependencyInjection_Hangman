0f2b4343f250af6c21602b94f6cac3a77f16e2da
ManuelSuarez07 <111906082+ManuelSuarez07@users.noreply.github.com>
2024-02-15 17:38:11 -0500

    Update OriginalScore.java

--- src/main/java/hangman/model/OriginalScore.java
+++ src/main/java/hangman/model/OriginalScore.java
@@ -1,3 +1,4 @@
+package hangman.model
 public class OriginalScore implements GameScore {
     @Override
     public int calculateScore(int correctCount, int incorrectCount) throws IllegalArgumentException {
