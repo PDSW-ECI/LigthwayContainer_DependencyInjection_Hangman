5bfccb2e14dbd6b06fcfea221d336e35acf51468
ManuelSuarez07 <111906082+ManuelSuarez07@users.noreply.github.com>
2024-02-15 17:37:30 -0500

    Update BonusScore.java

--- src/main/java/hangman/model/BonusScore.java
+++ src/main/java/hangman/model/BonusScore.java
@@ -1,3 +1,4 @@
+package hangman.model
 public class BonusScore implements GameScore {
     @Override
     public int calculateScore(int correctCount, int incorrectCount) throws IllegalArgumentException {
