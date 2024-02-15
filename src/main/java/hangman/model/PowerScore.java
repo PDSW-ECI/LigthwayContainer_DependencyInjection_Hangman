9cce01ace2c82975af299f44230466fb54529757
ManuelSuarez07 <111906082+ManuelSuarez07@users.noreply.github.com>
2024-02-15 17:51:45 -0500

    Update PowerScore.java

--- src/main/java/hangman/model/PowerScore.java
+++ src/main/java/hangman/model/PowerScore.java
@@ -1,12 +1,24 @@
-package hangman.model;
+package hangman.model; 
+import java.lang.Math;
 public class PowerScore implements GameScore {
+    private int score ;
+    private int failure= 8;
+    private int bonus= 5;
+    private int minScore= 0;
+    private int maxScore =500;
     @Override
-    public int calculateScore(int correctCount, int incorrectCount) throws IllegalArgumentException {
-        if (correctCount < 0 || incorrectCount < 0) {
-            throw new IllegalArgumentException("Los contadores de letras correctas e incorrectas no pueden ser negativos");
+    public int calculateScore(int correctCount, int incorrectCount) throws GameScoreException {
+        score = 0;
+        if( correctCount < 0 || incorrectCount <0 ) {
+            throw new GameScoreException(GameScoreException.NEGATIVE_NUMBERS);
         }
-        int score = correctCount * 5 - (incorrectCount * 8);
-        score = Math.max(score, 0);
-        return Math.min(score, 500); // Máximo puntaje permitido es 500
+ 
+        for( int i=1 ; i <=correctCount; i++){
+            score+= (Math.pow(bonus,i));
+        }
+ 
+        score =( score - (failure*incorrectCount) < minScore )? minScore :  score - (failure*incorrectCount);
+        score =( score > maxScore )? maxScore :  score ;
+        return score;
     }
 }
