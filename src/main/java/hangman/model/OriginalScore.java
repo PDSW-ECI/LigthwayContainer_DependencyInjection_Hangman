e3a5cdf6f05d44ac85b3a9041e141e6193d6cc5e
ManuelSuarez07 <111906082+ManuelSuarez07@users.noreply.github.com>
2024-02-15 17:49:48 -0500

    Update OriginalScore.java

--- src/main/java/hangman/model/OriginalScore.java
+++ src/main/java/hangman/model/OriginalScore.java
@@ -1,11 +1,15 @@
 package hangman.model;
-public class OriginalScore implements GameScore {
+public class OriginalScore implements GameScore{
+    private int score ;
+    private int failure=10;
+    private int minScore=0;
     @Override
-    public int calculateScore(int correctCount, int incorrectCount) throws IllegalArgumentException {
-        if (correctCount < 0 || incorrectCount < 0) {
-            throw new IllegalArgumentException("Los contadores de letras correctas e incorrectas no pueden ser negativos");
+    public int calculateScore(int correctCount, int incorrectCount) throws GameScoreException {
+        score = 100;
+        if( correctCount < 0 || incorrectCount <0 ){
+            throw new GameScoreException( GameScoreException.NEGATIVE_NUMBERS);
         }
-        int score = 100 - (incorrectCount * 10);
-        return Math.max(score, 0);
+        score =( score - (failure*incorrectCount) < minScore )? minScore :  score - (failure*incorrectCount);
+        return score;
     }
 }
