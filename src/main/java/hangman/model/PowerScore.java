9562380e932b6a0adbc58353d9dfc8aa77d582b2
ManuelSuarez07 <111906082+ManuelSuarez07@users.noreply.github.com>
2024-02-15 17:38:26 -0500

    Update PowerScore.java

--- src/main/java/hangman/model/PowerScore.java
+++ src/main/java/hangman/model/PowerScore.java
@@ -1,3 +1,4 @@
+package hangman.model
 public class PowerScore implements GameScore {
     @Override
     public int calculateScore(int correctCount, int incorrectCount) throws IllegalArgumentException {
