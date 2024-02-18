eb0d2d64c92b6786ca256a33204e04a78926f930
ManuelSuarez07 <111906082+ManuelSuarez07@users.noreply.github.com>
2024-02-18 12:48:02 -0500

    Implementación del guice

--- src/main/java/hangman/setup/guice/HangmanFactoryServices.java
+++ src/main/java/hangman/setup/guice/HangmanFactoryServices.java
@@ -1,20 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hangman.setup.guice;
 
-/**
- * @author 2106913
- */
+import hangman.model.French;
+import hangman.model.GameScore;
+import hangman.model.Language;
+import hangman.model.PowerScore;
+import hangman.model.Spanish;
+import hangman.model.dictionary.HangmanDictionary;
+import hangman.model.dictionary.FrenchDictionaryDataSource;
+import hangman.view.HangmanNoviolentoPanel;
+import hangman.view.HangmanPanel;
 
 public class HangmanFactoryServices extends com.google.inject.AbstractModule {
-
     @Override
     protected void configure() {
         /* Guice dependency injection */
         // bind(Interface.class).to(Concrete.class);
+        bind(Language.class).to(Spanish.class);
+        bind(HangmanDictionary.class).to(FrenchDictionaryDataSource.class);
+        bind(HangmanPanel.class).to(HangmanNoviolentoPanel.class);
+        //bind(GameScore.class).to(BonusScore.class);
+        bind(GameScore.class).to(PowerScore.class);
     }
 
 }
